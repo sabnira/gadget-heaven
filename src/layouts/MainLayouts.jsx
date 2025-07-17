edd940ad9b854ecac35761a70cef6cebafcc87e1
sabnira <sabnirajeemi@gmail.com>
2025-07-17 20:04:56 +0600

    commit

---
+++ src/layouts/MainLayouts.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayouts = () => {
+    return (
+        <div>
+            this is layout
+        </div>
+    );
+};
+
+export default MainLayouts;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayouts from "../layouts/MainLayouts";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayouts></MainLayouts>,
+  },
+]);
+
+
+export default router;
